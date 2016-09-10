565db27d6ee024ee16f93a85249cddf16a72d3d4
EC2 Default User <ec2-user@ip-172-31-41-144.us-west-2.compute.internal>
2016-09-10 04:03:31 +0000

    moving things around to make this more than just a vim installer

---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# setup.sh Jeff W. Doak jeff.w.doak@gmail.com
+
+# Script to initialize my settings on a new server.
+
+me=`whoami`
+
+# initialize directories
+if [ ! -e ~/src ]
+then
+  mkdir ~/src
+fi
+if [ ! -e ~/bin ]
+then
+  mkdir ~/bin
+fi
+
+# solarized color scheme
+git clone https://github.com/seebi/dircolors-solarized.git ~/src/dircolors-solarized/
+ln -s ~/src/dircolors-solarized/dircolors.256dark ~/.dircolors
+
+# setup bashrc
+
+
+
+# setup vimrc
+
+
+
+
