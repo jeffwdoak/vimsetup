a58cd0c5c2ade327168d4924b909d2695b4b1246
jeffwdoak <jeff.w.doak@gmail.com>
2017-12-07 21:26:42 -0600

    keep copies of old rc files

--- setup.sh
+++ setup.sh
@@ -21,11 +21,16 @@ git clone https://github.com/seebi/dircolors-solarized.git ~/src/dircolors-solar
 ln -s ~/src/dircolors-solarized/dircolors.256dark ~/.dircolors
 
 # setup bashrc
+cp ~/.bashrc ~/.bashrc_`date +%F`  # keep copy of old bashrc
 cp bashrc ~/.bashrc
 
 # setup vim
 cp -r vim ~/.vim
+cp ~/.vimrc ~/.vimrc_`date +%F`  # keep copy of old vimrc
+cp vim/vimrc ~/.vimrc
 
+# install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
 
